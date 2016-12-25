6bdc543d114648ab194a34d01147faf7c7541f0d
Karl Pettersson <karl_pettersson_1998@yahoo.com>
2016-12-25 20:28:38 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,130 @@
-# Seregmort
+# Seregmort 
 
 [![Build Status](https://travis-ci.org/klpn/Seregmort.jl.svg?branch=master)](https://travis-ci.org/klpn/Seregmort.jl)
 
 [![Coverage Status](https://coveralls.io/repos/klpn/Seregmort.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/klpn/Seregmort.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/klpn/Seregmort.jl/coverage.svg?branch=master)](http://codecov.io/github/klpn/Seregmort.jl?branch=master)
+
+This package can be used to analyze cause-specific regional Swedish mortality
+data at county or municipality level using the Statistics Sweden API ([API
+documentation in
+English](http://www.scb.se/Grupp/OmSCB/API/API-description.pdf)). The database
+accessible via the API covers deaths from 1969 to 1996 (a database covering
+cause-specific deaths at the county level occurring since 1997 is publicly
+accessible via [The National Board of Health and
+Welfare](http://www.socialstyrelsen.se/statistik/statistikdatabas/dodsorsaker)
+but cannot be used with the API). The package is a reimplementation in Julia of
+[seregmort](https://github.com/klpn/seregmort), which is written in Python.
+
+Data are retrieved in the [JSON-stat format](http://json-stat.org/) and saved
+into Julia DataFrames using the [JSONStat](https://github.com/klpn/JSONStat.jl)
+package.
+
+Possible values for regions, age groups, sexes and causes of death can be retrieved by `metadata()` which sends a GET request to the [mortality table](http://api.scb.se/OV0104/v1/doris/sv/ssd/START/HS/HS0301/DodaOrsak).
+
+If the cause of death given contains a hyphen, it is assumed to be a chapter
+rather than a single cause of death (the `agg:DödsorsakKapitel` level). The
+following multi-cause chapters are supported:
+
+| Chapter | Description
+| ------- | -----------
+| 1-2 | Infections
+| 3-16 | Tumors
+| 17-18 | Endocrine disorders
+| 20-21 | Mental disorders
+| 23-28 | Circulatory disorders
+| 29-32 | Respiratory disorders
+| 33-35 | Digestive disorders
+| 36-39 | Genitourinary disorders
+| 44-45 | Ill-defined causes
+| 46-52 | External causes
+
+##Examples
+Save data on deaths from circulatory disorders in Västmanland County for the
+whole period in a dictionary, and plot a smoothed diagram showing the time
+trend for proportion of deaths due to this cause group for females and males in
+age intervals above 70 years:
+
+```julia
+using Seregmort
+pardict = catot_yrsdict("19", "23-28")
+propplotyrs_dict("70-74", "90+", pardict)
+```
+Save data on deaths from circulatory disorders in all municipalities in
+Norrbotten County for the period 1981--86, and make a scatterplot of female vs
+male proportion of all deaths due to this cause group during the period in the
+age intervals between 75--79 and 85--89 years: 
+```julia
+pardict = catot_mapdict(munis_incounty("25", metadata()), "23-28", 1981, 1986)
+propscatsexes_dict("75-79", "85-89", pardict)
+```
+Note that data for single years and narrow age bands are often not very useful due to the small numbers of deaths, especially at the municipality level.
+
+Using [cartopy](https://github.com/SciTools/cartopy), it is also possible to plot maps showing regions with a lower or higher proportion of deaths from a given cause. 
+
+The script has been adapted to work with the shapefiles available (under a
+CCZero license) from National Archives of Sweden. You may download a [ZIP
+archive](http://riksarkivet.se/psi/NAD_Topografidata.zip) with these files and
+unzip it in the directory `naddata` under the `seregmort` directory. Data from
+[one of the Excel metadata files](http://riksarkivet.se/psi/g_units_names.xls)
+is included in this repository in JSON format (under `data`); this file is used
+to translate the geographical codes used by Statistics Sweden into the unit
+codes used in the shapefiles.
+
+Plot a map of the proportion of female deaths due to circulatory disorders in
+all municipalities in Västernorrland County during the period 1981--86 in the
+age intervals between 75--79 and 85--89 years (note that
+`data/2504/__pgsql2shp2504_tmp_table.shp` is default shapefile path):
+```julia
+pardict = catot_mapdict(munis_incounty("22", metadata()), "23-28", 1981, 1986)
+propmap_dict("75-79", "85-89", "2", pardict)
+```
+
+By default, maps are plotted with three percentiles with different shades. You
+can call the functions `fourp` and `fivep` to plot with four or five
+percentiles instead. To plot a map of proportion of deaths due to ischemic
+heart disease for all counties and all ages during the period 1981--86 among
+males with five percentiles:
+```julia
+pardict = catot_mapdict(allregions("county", metadata()), "25", 1981, 1986)
+propmap_dict("0", "90+", "1", pardict, fivep)
+```
+
+There is limited support for visualizing mortality rates by using population
+size in the denominator (based on data from a [population
+table](http://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101A/BefolkningNy).
+However, this is difficult to implement fully, because the tables use differing
+age formats and (more importantly) because the population table uses a newer
+regional division. Currently, it should work for age groups between 5--9 and
+85--89 years and regions which have not changed since 1996. For example, to
+plot a map of male mortality rates from circulatory disorders in the municipalities
+in Västerbotten County during the period 1981--86 in the age intervals between
+50--54 and 70--74 years:
+```julia
+pardict = capop_mapdict(munis_incounty("24", metadata(morturl)), "23-28", 1981, 1986)
+propmap_dict("50-54", "70-74", "1", pardict)
+```
+
+The `capop_mapdict` wrapper can also be used with `propscatsexes_dict` for drawing
+scatterplots, as in the example with `catot_mapdict`. The `capop_yrsdict`
+wrapper can be used with `propplotyrs_dict`.
+
+The `unchanged_regions` function can be used to selection those counties which
+have remained unchanged. To plot a map of female mortality rates from circulatory
+disorders in all counties, except for the changed ones in what is nowadays
+Skåne and Västra Götaland County, during the period 1981--86 in the age intervals
+65--69 and 70--74 years:
+```julia
+pardict = capop_mapdict(unchanged_regions("county", metadata()), "23-28", 1981, 1986)
+propmap("65-69", "70-74", pardict, "2")
+```
+
+Death rates may be influenced by the age structure of a population, which may
+give misleading results, especially if wide age bands are compared. Because of
+this, it is possible to calculate average death rates over 5-year age
+intervals, by setting the argument `agemean` to `true`, for example:
+```julia
+pardict = capop_mapdict(unchanged_regions("county", metadata()), "23-28", 1981, 1986)
+propmap("15-19", "70-74", "2", fivep, true)
+```
--- src/Seregmort.jl
+++ src/Seregmort.jl
@@ -1,5 +1,441 @@
 module Seregmort
 
-# package code goes here
+using JSONStat, Requests, DataStructures, DataFrames, PyCall, PyPlot
+import JSON
+@pyimport cartopy.io.shapereader as shpreader
+@pyimport cartopy.crs as ccrs
+export ndeaths, npop, propplot_dict, propplot, propscatsexes_dict, propscatsexes,
+propmap_dict, propmap, catot_yrsdict, capop_yrsdict, catot_mapdict, capop_mapdict,
+threep, fourp, fivep
+
+MORTURL = "http://api.scb.se/OV0104/v1/doris/sv/ssd/START/HS/HS0301/DodaOrsak"
+POPURL = "http://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101A/BefolkningNy"
+G_UNITS = JSON.parsefile("data/g_units.json")
+
+PyDict(matplotlib["rcParams"])["axes.formatter.use_locale"] = true
+matplotlib[:style][:use]("ggplot")
+
+function scb_to_unit(scb)
+	scbform = *("SE/", rpad(scb, 9, 0))
+	if scbform in keys(G_UNITS)
+		return G_UNITS[scbform]
+	else
+		return 0
+	end
+end
+
+function metadata(url = MORTURL)
+	req = Requests.get(url)
+	JSON.parse(Requests.readall(req), dicttype = DataStructures.OrderedDict)
+end
+
+function causealias(cause, dim)
+	if cause == "POP"
+		return lowercase(dim["ContentsCode"]["category"]["label"]["BE0101N1"])
+	else
+		return dim["Dodsorsak"]["category"]["label"][cause]
+	end
+end
+
+regalias(region, dim) = lstrip(replace(dim["Region"]["category"]["label"][region], region, ""))
+
+sexalias(sex, dim) = dim["Kon"]["category"]["label"][sex]
+
+function agesplitter(age)
+	if contains(age, "-")
+	    return split(age, "-")
+	else
+	    return [age]
+	end
+end
+
+function ageslice(startage, endage, agemean)
+	ages = allages()
+	startind = indexin([startage], ages)[1]
+	endind = indexin([endage], ages)[1]
+	if agemean
+		agemeanstr = " medel över åldrar"
+	else
+		agemeanstr = ""
+	end
+	if startage == endage
+		alias = *(replace(startage, "-", '\u2013'), agemeanstr)
+	else
+	    alias = *(agesplitter(startage)[1], "\u2013", agesplitter(endage)[end], agemeanstr)
+	end
+	agelist = ages[startind:endind]
+	Dict("agelist" => agelist, "alias" => alias)
+end
+
+function allages(ageformat = "mort")
+	if ageformat == "mort"
+		startint = "0"
+		startages = vcat(1, collect(5:5:85))
+		endages = vcat(4, collect(9:5:89))
+		endint = "90+"
+	elseif ageformat == "pop"
+		startint = "-4"
+		startages = collect(5:5:95)
+		endages = collect(9:5:99)
+		endint = "100+"
+	end
+	midints = ["$x-$y" for (x, y) in zip(startages, endages)]
+	vcat(startint, midints, endint)
+end
+
+function allregions(level, metadict)
+	regvalues = metadict["variables"][1]["values"]
+	if level == "county"
+		return filter(is_county, regvalues)
+	elseif level == "municipality"
+		return filter(is_municipality, regvalues)
+	end
+end
+
+unchanged_county(region) = !(region[1:2] in ["11"; "12"; "14"; "15"; "16"])
+unchanged_regions(level, metadict) = filter(unchanged_county, allregions(level, metadict))
+
+yearrange(startyear = 1969, endyear = 1996) = ["$(year)" for year in startyear:endyear]
+
+is_county(region) = length(region) == 2 && region != "00"
+is_municipality(region) = length(region) == 4
+
+function munis_incounty(county, metadict)
+	regvalues = metadict["variables"][1]["values"]
+	filter((x) -> (is_municipality(x) && startswith(x, county)), regvalues)
+end
+
+function mortreqjson(regvalues, causevalues,  agevalues = allages(),
+	sexvalues = ["1"; "2"], yearvalues = yearrange())
+	if is_county(regvalues[1])
+		regfilter = "vs:RegionLän"
+	else
+		regfilter = "vs:RegionKommun95"
+	end
+	if contains(causevalues[1], "-")
+		causefilter = "agg:DödsorsakKapitel"
+	else
+		causefilter = "item"
+	end
+
+	Dict("response" => Dict("format" => "json-stat"),
+		"query" => [
+		Dict("selection" => Dict("filter" => regfilter, "values" => regvalues),
+			"code" => "Region");
+		Dict("selection" => Dict("filter" => causefilter, "values" => causevalues),
+			"code" => "Dodsorsak");
+		Dict("selection" => Dict("filter" => "item", "values" => agevalues),
+			"code" => "Alder");
+		Dict("selection" => Dict("filter" => "item", "values" => sexvalues),
+			"code" => "Kon");
+		Dict("selection" => Dict("filter" => "item", "values" => yearvalues),
+			"code" => "Tid")
+		]
+		)
+end
+
+function popreqjson(regvalues, agevalues = allages("pop"),
+	sexvalues = ["1"; "2"], yearvalues = yearrange())
+	if is_county(regvalues[1])
+		regfilter = "vs:RegionLän07"
+	else
+		regfilter = "vs:RegionKommun07"
+	end
+
+	Dict("response" => Dict("format" => "json-stat"),
+		"query" => [
+		Dict("selection" => Dict("filter" => regfilter, "values" => regvalues),
+			"code" => "Region");
+		Dict("selection" => Dict("filter" => "agg:Ålder5år", "values" => agevalues),
+			"code" => "Alder");
+		Dict("selection" => Dict("filter" => "item", "values" => sexvalues),
+			"code" => "Kon");
+		Dict("selection" => Dict("filter" => "item", "values" => ["BE0101N1"]),
+			"code" => "ContentsCode");
+		Dict("selection" => Dict("filter" => "item", "values" => yearvalues),
+			"code" => "Tid")
+		]
+		)
+end
+
+function ndeaths(regvalues, causevalues;  agevalues = allages(),
+	sexvalues = ["1"; "2"], yearvalues = yearrange())
+	qjson = mortreqjson(regvalues, causevalues, agevalues, sexvalues, yearvalues)
+	req = Requests.post(MORTURL, json = qjson)
+	reqjsonstat = JSON.parse(Requests.readall(req),
+		dicttype = DataStructures.OrderedDict)
+	readjsonbundle(reqjsonstat)["dataset"]
+end
+
+function npop(regvalues; agevalues = allages("pop"),
+	sexvalues = ["1"; "2"], yearvalues = yearrange())
+	qjson = popreqjson(regvalues, agevalues, sexvalues, yearvalues)
+	req = Requests.post(POPURL, json = qjson)
+	reqjsonstat = JSON.parse(Requests.readall(req),
+		dicttype = DataStructures.OrderedDict)
+	readjsonbundle(reqjsonstat)["dataset"]
+end
+
+dfarrmatch(col, arr) = map((x) -> in(x, arr), Vector(col))
+
+subframe_sray(df, sex, region, agelist, years) = df[((df[:Kon].==sex)
+	& (df[:Region].==region) & (dfarrmatch(df[:Alder], agelist))
+	& (dfarrmatch(df[:Tid], years))), :]
+
+dfgrp(df, grpcol, f = sum) = by(df, grpcol, x -> DataFrame(value = f(x[:value])))
+
+function grpprop(numframe_sub, denomframe_sub, grpcol, agemean)
+	if agemean
+		propfr_agesp = DataFrame()
+		propfr_agesp[grpcol] = numframe_sub[grpcol]
+		propfr_agesp[:value] = numframe_sub[:value]./denomframe_sub[:value]
+		return dfgrp(propfr_agesp, grpcol, mean)
+	else
+		numgrp = dfgrp(numframe_sub, grpcol)
+		denomgrp = dfgrp(denomframe_sub, grpcol)
+		propfr_agegr = DataFrame()
+		propfr_agegr[grpcol] = numgrp[grpcol]
+		propfr_agegr[:value] = numgrp[:value]./denomgrp[:value]
+		return propfr_agegr
+	end
+end
+
+function prop_timegrp(numframe, denomframe, sex, region, agelist, years, agemean)
+	numframe_sub = subframe_sray(numframe, sex, region, agelist, years)
+	denomframe_sub = subframe_sray(denomframe, sex, region, agelist, years)
+	grpprop(numframe_sub, denomframe_sub, :Tid, agemean)
+
+end
+
+propplotyrs_dict(startage, endage, pardict,
+	agemean = false, years = yearrange(), sexes = ["2", "1"]) =
+propplotyrs(pardict["numframe"], pardict["denomframe"], pardict["numdim"],
+	pardict["denomdim"], pardict["numcause"], pardict["denomcause"], pardict["region"],
+	startage, endage, agemean, years, sexes)
+
+function propplotyrs(numframe, denomframe, numdim, denomdim, numcause, denomcause, 
+        region, startage, endage, agemean, years, sexes)
+	numcauseal = causealias(numcause, numdim)
+	denomcauseal = causealias(denomcause, denomdim)
+	regal = regalias(region, numdim)
+	ages = ageslice(startage, endage, agemean)
+	ageal = ages["alias"]
+	agelist = ages["agelist"]
+	yrints = map((x)->parse(Int, x), years)
+	for sex in sexes
+		sexal = sexalias(sex, numdim)
+		propframe = prop_timegrp(numframe, denomframe, 
+			sex, region, agelist, years, agemean) 
+		plot(yrints, propframe[:value], label = sexal)
+	end
+	legend(framealpha = 0.5)
+	xlim(yrints[1], yrints[end])
+	ylim(ymin = 0)
+	title("Döda $(numcauseal)/$(denomcauseal)\n$(ageal) $(regal)")
+end
+
+subframe_sa(df, sex, agelist) = df[((df[:Kon].==sex)
+	& (dfarrmatch(df[:Alder], agelist))), :]
+
+function prop_reggrp(numframe, denomframe, sex, agelist, agemean)
+	numframe_sub = subframe_sa(numframe, sex, agelist)
+	denomframe_sub = subframe_sa(denomframe, sex, agelist)
+	grpprop(numframe_sub, denomframe_sub, :Region, agemean)
+end
+
+propscatsexes_dict(startage, endage, pardict, agemean = false) =
+propscatsexes(pardict["numframe"], pardict["denomframe"], pardict["numdim"],
+	pardict["denomdim"], pardict["numcause"], pardict["denomcause"],
+	startage, endage, agemean)
+
+function propscatsexes(numframe, denomframe, numdim, denomdim, numcause, denomcause, 
+        startage, endage, agemean = false)
+	numcauseal = causealias(numcause, numdim)
+	denomcauseal = causealias(denomcause, denomdim)
+	ages = ageslice(startage, endage, agemean)
+	ageal = ages["alias"]
+	agelist = ages["agelist"]
+	yrints = map((x)->parse(Int, x), numframe[:Tid])
+	startyear = minimum(yrints)
+	endyear = maximum(yrints)
+	sexframes = Dict()
+	for sex in ["2", "1"]
+		sexframes[sex] = Dict()
+		sexframes[sex]["alias"] = sexalias(sex, numdim)
+		sexframes[sex]["propframe"] = prop_reggrp(numframe, denomframe,
+			sex, agelist, agemean)
+	end
+	femvals = sexframes["2"]["propframe"][:value]
+	malevals = sexframes["1"]["propframe"][:value]
+	scatter(femvals, malevals)
+	for (i, regcode) in enumerate(sexframes["2"]["propframe"][:Region])
+		annotate(regcode, (femvals[i], malevals[i]))
+	end
+	xlabel(sexframes["2"]["alias"])
+	ylabel(sexframes["1"]["alias"])
+	axminimum = 0.95
+	axmaximum = 1.05
+	xlim(minimum(femvals)*axminimum, maximum(femvals)*axmaximum)
+	ylim(minimum(malevals)*axminimum, maximum(malevals)*axmaximum)
+	title(*("Döda $(numcauseal)/$(denomcauseal)\n",
+		"$(ageal) $(startyear)\u2013$(endyear)"))
+end
+
+perc_round(value) = replace("$(round(value, 4))", ".", ",")
+
+threep(prop) = 
+	[
+	Dict("col" => "lightsalmon", "value" => percentile(prop, 1/3*100));
+	Dict("col" => "tomato", "value" => percentile(prop, 2/3*100));
+	Dict("col" => "red", "value" => percentile(prop, 100))
+	]
+
+fourp(prop) = 
+	[
+	Dict("col" => "lightyellow", "value" => percentile(prop, 1/4*100));
+	Dict("col" => "yellow", "value" => percentile(prop, 2/4*100));
+	Dict("col" => "tomato", "value" => percentile(prop, 3/4*100));
+	Dict("col" => "red", "value" => percentile(prop, 100))
+	]
+
+fivep(prop) = 
+	[
+	Dict("col" => "lightyellow", "value" => percentile(prop, 1/5*100));
+	Dict("col" => "yellow", "value" => percentile(prop, 2/5*100));
+	Dict("col" => "orange", "value" => percentile(prop, 3/5*100));
+	Dict("col" => "tomato", "value" => percentile(prop, 4/5*100));
+	Dict("col" => "red", "value" => percentile(prop, 100))
+	]
+
+propmap_dict(startage, endage, sex, pardict, percfunc = threep, agemean = false) =
+propmap(pardict["numframe"], pardict["denomframe"], pardict["numdim"],
+	pardict["denomdim"], pardict["numcause"], pardict["denomcause"],
+	startage, endage, sex, pardict["shapefname"], percfunc, agemean)
+
+function propmap(numframe, denomframe, numdim, denomdim, numcause, denomcause,
+        startage, endage, sex, shapefname, percfunc = threep, agemean = false)
+	numcauseal = causealias(numcause, numdim)
+	denomcauseal = causealias(denomcause, denomdim)
+	sexal = sexalias(sex, numdim)
+	ages = ageslice(startage, endage, agemean)
+	ageal = ages["alias"]
+	agelist = ages["agelist"]
+	yrints = map((x)->parse(Int, x), numframe[:Tid])
+	startyear = minimum(yrints)
+	endyear = maximum(yrints)
+	region_shp = shpreader.Reader(shapefname)
+	propframe = prop_reggrp(numframe, denomframe, sex, agelist, agemean)
+	regvalues = propframe[:Region]
+	prop = propframe[:value]
+	propdict = Dict(zip(regvalues, prop))
+	units = map(scb_to_unit, regvalues)
+	regdict = Dict(zip(units, regvalues))
+	percentiles = percfunc(prop) 
+	ax = plt[:axes](projection = ccrs.TransverseMercator())
+	boundlist = []
+	facecolor = "red"
+	for region_rec in region_shp[:records]()
+		regcode = region_rec[:attributes]["G_UNIT"]
+		regend = region_rec[:attributes]["GET_END_YE"]
+		if (regcode in keys(regdict) && regend > 1995)
+			boundlist = vcat(boundlist, region_rec[:bounds])
+			for percentile in percentiles
+				if propdict[regdict[regcode]] <= percentile["value"]
+					facecolor = percentile["col"]
+					break
+				end
+			end
+			ax[:add_geometries](region_rec[:geometry],
+				ccrs.TransverseMercator(),
+				edgecolor = "black", facecolor = facecolor)
+		end
+	end
+	xminimum = minimum([bound[1] for bound in boundlist])
+	xmaximum = maximum([bound[3] for bound in boundlist])
+	yminimum = minimum([bound[2] for bound in boundlist])
+	ymaximum = maximum([bound[4] for bound in boundlist])
+	ax[:set_xlim](xminimum, xmaximum)
+	ax[:set_ylim](yminimum, ymaximum)
+	percpatches = []
+	perclabels = []
+	for (i, percentile) in enumerate(percentiles)
+		percpatch = matplotlib[:patches][:Rectangle]((0, 0), 1, 1,
+			facecolor = percentile["col"])
+		percpatches = vcat(percpatches, percpatch)
+		if i == 1
+			perclabel = *("\u2265", perc_round(minimum(prop)),
+				"\n\u2264", perc_round(percentile["value"]))
+		else
+			perclabel = *("\u2264", perc_round(percentile["value"]))
+		end
+		perclabels = vcat(perclabels, perclabel)
+	end
+	legend(percpatches, perclabels, loc = "upper left", 
+		framealpha = 0.75, bbox_to_anchor=(1,1))
+	title(*("Döda $(numcauseal)/$(denomcauseal)\n",
+		"$(sexal) $(ageal) $(startyear)\u2013$(endyear)"))
+	show()
+end
+
+function catot_yrsdict(region, cause)
+	cadeaths = ndeaths([region], [cause])
+	totdeaths = ndeaths([region], ["TOT"])
+	Dict(
+		"numframe" => cadeaths["datasetframe"],
+		"denomframe" => totdeaths["datasetframe"],
+		"numdim" => cadeaths["dimension"],
+		"denomdim" => totdeaths["dimension"],
+		"numcause" => cause,
+		"denomcause" => "TOT",
+		"region" => region
+	)
+end
+
+function capop_yrsdict(region, cause)
+	cadeaths = ndeaths([region], [cause])
+	pop = npop([region])
+	Dict(
+		"numframe" => cadeaths["datasetframe"],
+		"denomframe" => pop["datasetframe"],
+		"numdim" => cadeaths["dimension"],
+		"denomdim" => pop["dimension"],
+		"numcause" => cause,
+		"denomcause" => "POP",
+		"region" => region
+	)
+end
+
+function catot_mapdict(regvalues, cause, startyear, endyear,
+	shapefname = "data/2504/__pgsql2shp2504_tmp_table.shp")
+	cadeaths = ndeaths(regvalues, [cause],
+		yearvalues = yearrange(startyear, endyear))
+	totdeaths = ndeaths(regvalues, ["TOT"],
+		yearvalues = yearrange(startyear, endyear))
+	Dict(
+		"numframe" => cadeaths["datasetframe"],
+		"denomframe" => totdeaths["datasetframe"],
+		"numdim" => cadeaths["dimension"],
+		"denomdim" => totdeaths["dimension"],
+		"numcause" => cause,
+		"denomcause" => "TOT",
+		"shapefname" => shapefname 
+	)
+end
+
+function capop_mapdict(regvalues, cause, startyear, endyear,
+	shapefname = "data/2504/__pgsql2shp2504_tmp_table.shp")
+	cadeaths = ndeaths(regvalues, [cause],
+		yearvalues = yearrange(startyear, endyear))
+	pop = npop(regvalues, yearvalues = yearrange(startyear, endyear))
+	Dict(
+		"numframe" => cadeaths["datasetframe"],
+		"denomframe" => totdeaths["datasetframe"],
+		"numdim" => cadeaths["dimension"],
+		"denomdim" => totdeaths["dimension"],
+		"numcause" => cause,
+		"denomcause" => "POP",
+		"shapefname" => shapefname 
+	)
+end
 
 end # module
